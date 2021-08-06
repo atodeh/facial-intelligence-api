4fd87c6c7f74ae61bf69a734d2fe4a08cea12012
Ahmad Odeh <79285401+atodeh@users.noreply.github.com>
2021-08-06 16:15:22 -0400

    Adding database connection

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'facial_intelligence'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
