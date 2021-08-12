5356dc525c664b7f656fd5896d3e08286fc0cd20
Ahmad Odeh <79285401+atodeh@users.noreply.github.com>
2021-08-12 13:08:27 -0400

    Added SQL statements

--- SQL/database.sql
+++ SQL/database.sql
@@ -8,6 +8,6 @@ CREATE TABLE users (
 
 CREATE TABLE login (
   ID serial PRIMARY KEY,
-	hash VARCHAR(100) NOT NULL,
-	email text UNIQUE NOT NULL
+  hash VARCHAR(100) NOT NULL,
+  email text UNIQUE NOT NULL
 );
